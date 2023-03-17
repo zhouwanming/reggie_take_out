9b809f65ca2738b08583dc5802be3da36e4f79ac
zhouwanming <107608094+zhouwanming@users.noreply.github.com>
2023-03-18 03:41:57 +0800

    Update ReggieApplication.java

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -14,7 +14,7 @@ public class ReggieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
-        log.info("项目启动1");
+        log.info("项目启动12");
     }
 
 }
