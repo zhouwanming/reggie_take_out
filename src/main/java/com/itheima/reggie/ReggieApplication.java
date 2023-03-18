bf2c519213e6e0fea03da579accd57fe5ad42b88
zwm <3295260031@qq.com>
2023-03-18 23:24:58 +0800

    push commit

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -14,7 +14,7 @@ public class ReggieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class,args);
-        log.info("项目启动12");
+        log.info("项目启动");
     }
 
 }
